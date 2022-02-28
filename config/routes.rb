57b06ca9fdbe197ebbab780503d4c2b1aae4af3c
Simon Charbonnier <simoncharbonnier@orange.fr>
2022-02-28 14:28:14 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :trips, only: [:index, :show, :create, :update] do
+    resources :subscriptions, only: [:create, :update, :destroy]
+    resources :availabilities, only: :create
+    resources :places, only: :update
+    resources :placeproposals, only: :create
+    resources :messages, only: :create
+    resources :tasks, only: [:create, :update]
+  end
 end
