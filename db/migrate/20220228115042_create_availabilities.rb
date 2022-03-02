2c22044eb5496aa29630789098ae1d271e41427b
Simon Charbonnier <simoncharbonnier@orange.fr>
2022-03-02 12:37:31 +0100

    db maj

--- db/migrate/20220228115042_create_availabilities.rb
+++ db/migrate/20220228115042_create_availabilities.rb
@@ -1,8 +1,7 @@
 class CreateAvailabilities < ActiveRecord::Migration[6.1]
   def change
     create_table :availabilities do |t|
-      t.date :start_at
-      t.date :end_at
+      t.date :availability
       t.references :user, null: false, foreign_key: true
       t.references :trip, null: false, foreign_key: true
 
