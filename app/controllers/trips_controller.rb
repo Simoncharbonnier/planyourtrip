3b976a44bb492537dd4d28c39304714a4981d2b1
Simon Charbonnier <simoncharbonnier@orange.fr>
2022-03-02 16:55:07 +0100

    form-trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,14 +1,23 @@
 class TripsController < ApplicationController
   def index
+    @trip = Trip.new
     @my_trips = current_user.trips
     @my_pending_trips = @my_trips.select { |trip| trip.status == "pending" }
     @my_confirmed_trips = @my_trips.select { |trip| trip.status == "confirmed" }
     @my_passed_trips = @my_trips.select { |trip| trip.status == "passed" }
   end
 
-  def new
-    @availabilities = Availability.new
-    @place_proposal = PlaceProposal.new
-    @trip = Trip.new
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.user_id = current_user.id
+    @trip.save
+
+    redirect_to new_trip_availability_path(@trip)
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name, :duration)
   end
 end
--- db/migrate/20220228115042_create_availabilities.rb
+++ db/migrate/20220228115042_create_availabilities.rb
@@ -1,7 +1,7 @@
 class CreateAvailabilities < ActiveRecord::Migration[6.1]
   def change
     create_table :availabilities do |t|
-      t.date :availability
+      t.string :availability
       t.references :user, null: false, foreign_key: true
       t.references :trip, null: false, foreign_key: true
 
