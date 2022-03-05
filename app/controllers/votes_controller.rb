da9991c3c5a183aa6c15fe449e3a250dee19a5b4
Simon Charbonnier <simoncharbonnier@orange.fr>
2022-03-05 15:17:11 +0100

    orga + votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,17 @@
 class VotesController < ApplicationController
   def create
-    @trip = Trip.find(params[:trip_id])
+    @trip = Trip.find(place_params[:trip_id])
+    @vote = Vote.new()
+    @vote.place_proposal_id = place_params[:pp]
+    @vote.user = current_user
+    @vote.save
+
+    redirect_to trips_path
+  end
+
+  private
+
+  def place_params
+    params.permit(:pp, :trip_id)
   end
 end
