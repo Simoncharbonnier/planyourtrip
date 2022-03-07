fda5b1dd0ab99a1bcdd50900dcfdf8696060a03d
Valentin Derrien <derrien.v@gmail.com>
2022-03-07 12:49:29 +0100

    Added chat and messages to Trip show

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,19 @@
 class MessagesController < ApplicationController
+  def create
+    @trip = Trip.find(params[:trip_id])
+    @message = Message.new(message_params)
+    @message.trip = @trip
+    @message.user = current_user
+    if @message.save
+      redirect_to trip_path(@trip, anchor: "message-#{@message.id}")
+    else
+      render "trip/show"
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :time_span)
+  end
 end
