6b116e920c021234c018f7d6fa395bb2ff25cf77
Simon Charbonnier <simoncharbonnier@orange.fr>
2022-03-07 13:14:42 +0100

    swiper

---
+++ app/javascript/plugins/swiper.js
@@ -0,0 +1,16 @@
+// import Swiper JS
+import Swiper, { Navigation } from 'swiper';
+// import Swiper styles
+
+const initSwiper = () => {
+  const swiper = new Swiper('.swiper', {
+    modules: [Navigation],
+    direction: 'horizontal',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+}
+
+export { initSwiper };
