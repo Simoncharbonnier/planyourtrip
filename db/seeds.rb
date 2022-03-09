5fd5683dba909ce73443e878d9d73a441f868045
Simon Charbonnier <simoncharbonnier@orange.fr>
2022-03-09 20:59:31 +0100

    seed/er

--- db/seeds.rb
+++ db/seeds.rb
@@ -80,7 +80,7 @@ p "Users seed done !"
 
 # trips
 trip1 = Trip.create!(name: "Weekend aux Pays-Bas", user: simon, month: 9, status: "confirmed", time_span: "Weekend")
-trip2 = Trip.create!(name: "Aout chez les Grecs", user: valentin, month: 8, status: "confirmed", time_span: "Semaine")
+trip2 = Trip.create!(name: "Août chez les Grecs", user: valentin, month: 8, status: "confirmed", time_span: "Semaine")
 # trip3 = Trip.create!(name: "Voyage entre potes", user: fabien, month: 8, status: "voting", time_span: "Week")
 trip4 = Trip.create!(name: "Avril 2022", user: fabien, month: 4, status: "voting", time_span: "Weekend")
 
